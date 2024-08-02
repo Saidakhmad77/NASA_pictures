b4b97fce843d58041e9d8a26fa4259ba7af7fe44
Saidakhmad77 <lionuz669@gmail.com>
2024-08-02 16:29:05 +0900

    init commit

---
+++ nasa.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = 'uX5mbnMNcWqnpHauI0SB1fDmXaTN1IlbvB0unu3Q'; // Replace with your API key
+    const apiUrl = 'https://api.nasa.gov/planetary/apod';
+
+    // Function to fetch and display APOD
+    function updateAPOD(date) {
+        let url = apiUrl + '?api_key=' + apiKey;
+        
+        if (date) {
+            url += '&date=' + date;
+        }
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('apod-title').textContent = data.title;
+                document.getElementById('apod-description').textContent = data.explanation;
+                document.getElementById('apod-image').src = data.url;
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }
+
+    // Fetch today's APOD on initial load
+    updateAPOD();
+
+    // Event listener for date picker
+    document.getElementById('date-picker').addEventListener('change', (event) => {
+        const selectedDate = event.target.value;
+        updateAPOD(selectedDate);
+    });
+});
